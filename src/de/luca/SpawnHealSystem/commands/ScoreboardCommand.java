6f09803e98ebf6d86b5e2c92a2b98a2bdba251b3
LucaM2006 <72764739+LucaM2006@users.noreply.github.com>
2020-10-16 14:58:32 +0200

    Update ScoreboardCommand.java

--- src/de/luca/SpawnHealSystem/commands/ScoreboardCommand.java
+++ src/de/luca/SpawnHealSystem/commands/ScoreboardCommand.java
@@ -1,7 +1,3 @@
-// This class was created by LucaHD
-
-package de.luca.SpawnHealSystem.commands;
-
 import de.luca.SpawnHealSystem.main.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
@@ -20,22 +16,22 @@ public class ScoreboardCommand implements CommandExecutor {
             Player player = (Player) sender;
             if(player.hasPermission("sb.reload")) {
                 if(args.length == 1) {
-                    player.sendMessage(Main.getPrefix() + ("§eScoreBoard wird neu geladen..."));
+                    player.sendMessage(Main.getPrefix() + ("Â§eScoreBoard wird neu geladen..."));
                     try {
                         reload(player);
-                        player.sendMessage(Main.getPrefix() + ("§aScoreBoard wurde neu geladen!"));
+                        player.sendMessage(Main.getPrefix() + ("Â§aScoreBoard wurde neu geladen!"));
                     }catch (Exception e){
-                        player.sendMessage(Main.getPrefix() + ("§cFehler: " + e.getMessage()));
+                        player.sendMessage(Main.getPrefix() + ("Â§cFehler: " + e.getMessage()));
                     }
 
                 }else {
-                    player.sendMessage(Main.getPrefix() + ("§cBitte benutze: §6/sb reload"));
+                    player.sendMessage(Main.getPrefix() + ("Â§cBitte benutze: Â§6/sb reload"));
                 }
             }else {
-                player.sendMessage(Main.getPrefix() + ("§cDazu hast du keine Rechte!"));
+                player.sendMessage(Main.getPrefix() + ("Â§cDazu hast du keine Rechte!"));
             }
         } else {
-            sender.sendMessage(Main.getPrefix() + ("§cDu bist kein Spieler"));
+            sender.sendMessage(Main.getPrefix() + ("Â§cDu bist kein Spieler"));
         }
 
         return true;
@@ -45,14 +41,14 @@ public class ScoreboardCommand implements CommandExecutor {
         Scoreboard board = Bukkit.getScoreboardManager().getNewScoreboard();
         Objective objective = board.registerNewObjective("abcd", "abcd");
         objective.setDisplaySlot(DisplaySlot.SIDEBAR);
-        objective.setDisplayName("§6§lWillkommen auf DeinServer.de");
-        objective.getScore("§6§l--------------------------").setScore(6);
-        objective.getScore("§a§lHallo §6§l" + player.getName() + "§a§l!").setScore(5);
-        objective.getScore("§e§l----------------------").setScore(4);
-        objective.getScore("§a§lViel Spaß §6§l" + player.getName() + "§a§l!").setScore(3);
-        objective.getScore("§6§l-------------------------").setScore(2);
-        objective.getScore("§a§lgecodet von: §6§lLucaHD").setScore(1);
-        objective.getScore("§e§l--------------------------------").setScore(0);
+        objective.setDisplayName("Â§6Â§lWillkommen auf DeinServer.de");
+        objective.getScore("Â§6Â§l--------------------------").setScore(6);
+        objective.getScore("Â§aÂ§lHallo Â§6Â§l" + player.getName() + "Â§aÂ§l!").setScore(5);
+        objective.getScore("Â§eÂ§l----------------------").setScore(4);
+        objective.getScore("Â§aÂ§lViel SpaÃŸ Â§6Â§l" + player.getName() + "Â§aÂ§l!").setScore(3);
+        objective.getScore("Â§6Â§l-------------------------").setScore(2);
+        objective.getScore("Â§aÂ§lgecodet von: Â§6Â§lLucaHD").setScore(1);
+        objective.getScore("Â§eÂ§l--------------------------------").setScore(0);
         player.setScoreboard(board);
     }
 }
