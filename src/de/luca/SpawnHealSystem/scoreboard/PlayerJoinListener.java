a0cee935ef2def772871aa890bdc4e324b25e43e
LucaM2006 <72764739+LucaM2006@users.noreply.github.com>
2020-10-19 22:11:59 +0200

    Update PlayerJoinListener.java

--- src/de/luca/SpawnHealSystem/scoreboard/PlayerJoinListener.java
+++ src/de/luca/SpawnHealSystem/scoreboard/PlayerJoinListener.java
@@ -1,8 +1,3 @@
-// This class was created by LucaHD
-
-
-package de.luca.SpawnHealSystem.scoreboard;
-
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
